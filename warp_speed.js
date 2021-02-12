28cb4a1e98a5d3c4e0d0e54a2c45cad8bd0454b8
David Huffman <dshuffma@us.ibm.com>
2021-02-12 14:40:27 -0500

    fix loop offset

--- warp_speed.js
+++ warp_speed.js
@@ -85,6 +85,7 @@ module.exports = function (logger) {
 		// i'm doing this loop to limit the size of the doc stubs we keep in memory.
 		function millions_doc_loop(data, million_cb) {
 			data._doc_id_iter = data._doc_id_iter || 1;						// init if needed
+			data._skip_offset = data._skip_offset || 0;								// init if needed
 			if (data._doc_id_iter * MAX_STUBS_IN_MEMORAY >= 100e6) {		// don't recurse forever, at some point give up
 				logger.log('[loop] recursed on doc stubs for too long. giving up.', data._doc_id_iter);
 				return million_cb();
@@ -104,6 +105,7 @@ module.exports = function (logger) {
 
 					if (rec_doc_count > 0 && rec_doc_count === DOC_STUB_BATCH_SIZE) {	// if num of docs is the same as "limit" then there are more docs
 						data._doc_id_iter++;
+						data._skip_offset += MAX_STUBS_IN_MEMORAY;
 						logger.log('[phase 2] there are more docs to handle. going back to phase 1. i:', data._doc_id_iter, '\n');
 						return millions_doc_loop(data, million_cb);			// recurse
 					} else {
@@ -127,7 +129,7 @@ module.exports = function (logger) {
 				head_room_percent: options.head_room_percent,
 				_pause: false,
 				request_opts_builder: (iter) => {						// build the options for each batch clouant api
-					const skip = (iter - 1) * DOC_STUB_BATCH_SIZE;
+					const skip = (iter - 1) * DOC_STUB_BATCH_SIZE + data._skip_offset;
 					return {
 						method: 'GET',
 						baseUrl: options.db_connection,
@@ -256,14 +258,17 @@ module.exports = function (logger) {
 				predict_time_left(percent_docs);
 				write_docs_2_stream(api_id, docs);
 			}
+		}
 
-			// log much time is left
-			function predict_time_left(percent_docs) {
-				const job_elapsed_ms = Date.now() - start;
-				const estimated_total_ms = (percent_docs === 0) ? 0 : (1 / (percent_docs / 100) * job_elapsed_ms);
-				const time_left = estimated_total_ms - job_elapsed_ms;
-				logger.log('[estimates] total backup:', misc.friendly_ms(estimated_total_ms) + ', time left:', misc.friendly_ms(time_left));
-			}
+		// log much time is left for all loops to complete
+		function predict_time_left(percent_docs) {
+			const job_elapsed_ms = Date.now() - start;
+
+			// if it will take more than 1 loop, multiple the estimate by the number of loops & some fraction of the last loop
+			const loop_multiplier = (doc_count < MAX_STUBS_IN_MEMORAY) ? 1 : (doc_count / MAX_STUBS_IN_MEMORAY);
+			const estimated_total_ms = (percent_docs === 0) ? 0 : (1 / (percent_docs / 100) * job_elapsed_ms * loop_multiplier);
+			const time_left = (estimated_total_ms - job_elapsed_ms);
+			logger.log('[estimates] total backup:', misc.friendly_ms(estimated_total_ms) + ', time left:', misc.friendly_ms(time_left));
 		}
 
 		// write the docs to the write stream
@@ -335,12 +340,14 @@ module.exports = function (logger) {
 					const doc_count = resp1.doc_count;
 					const del_count = resp1.doc_del_count;
 					const seq = resp[1].last_seq;
+					const loops = Math.ceil(doc_count / MAX_STUBS_IN_MEMORAY);
 					logger.log('[stats] size:', misc.friendly_bytes(resp1.sizes.external));
 					logger.log('[stats] docs:', misc.friendly_number(doc_count));
 					logger.log('[stats] avg doc:', misc.friendly_bytes(avg_doc_bytes));
 					logger.log('[stats] batch size:', batch_size);
 					logger.log('[stats] deleted docs:', misc.friendly_number(del_count), '-', (del_count / (del_count + doc_count) * 100).toFixed(1) + '%');
-					return data_cb(null, { batch_size, seq, doc_count });						// pass the data we need on
+					logger.log('[stats] # phase loops:', loops);
+					return data_cb(null, { batch_size, seq, doc_count });					// pass the data we need on
 				}
 			});
 		}
